e8a9c0cf7c9009870fa0d05d59507278894bcda5
Indhu Reddy <indhureddychepakam@gmail.com>
2026-02-26 17:39:27 +0530

    added root path

--- main/urls.py
+++ main/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),   
+    path('', views.home, name='home'),   # 🔥 ADD THIS LINE
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
-    
     path('projects/', views.projects, name='projects'),
     path('skills/', views.skills, name='skills'),
     path("resume/", views.resume, name="resume"),
     path("contact/", views.contact, name="contact"),
-
 ]
